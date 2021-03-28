0065e3b212084e523ec163d32c37e395353185f7
Ekyoz <alexandre.tresallet@gmail.com>
2021-03-28 16:55:05 +0200

    add page2

--- app.py
+++ app.py
@@ -110,19 +110,18 @@ async def addroom(ctx, channel):
 
 @client.command()
 async def help(ctx):
-    embed = discord.Embed(title="Help", color=0x61aaf1)
-    embed.add_field(name="Change prefix",
-                    value="changeprefix [prefix]  ││  Change prefix bot", inline=False)
-    embed.add_field(name="Private rooms",
-                    value="addroom [id_channel]  ││  Create private room", inline=False)
-    embed.add_field(name="qzd", value="qzd", inline=False)
-    embed.set_footer(
-        text="Page 1/4", icon_url="https://cdn.discordapp.com/avatars/825696493037944882/db7f3d6bbe165222fc75b50402fcfdec.webp")
-    final_message = await ctx.send(embed=embed)
+    page1 = discord.Embed(title="Rooms", color=0x61aaf1)
+    page1.add_field(name="Create rooms", value="addroom [id_channel]  ││  Create private room", inline=False)
+    page1.add_field(name="Delete rooms", value="delroom [id_channel]  ││  Delete private room", inline=False)
+    page1.add_field(name="Rooms names", value="nameroom [id_channel] [name]  ││  Rename private room", inline=False)
+    page1.add_field(name="Private room", value="privroom [id_channel] [yes/no]  ││  Enable or disable private room", inline=False)
+    page1.set_footer(text="Page 1/4", icon_url="https://cdn.discordapp.com/avatars/825696493037944882/db7f3d6bbe165222fc75b50402fcfdec.webp")
+    page2 = discord.Embed(title="Commands", color=0x61aaf1)
+    page2.add_field(name="Change prefix", value="changeprefix [prefix]  ││  Change prefix bot", inline=False)
+    page2.set_footer(text="Page 2/4", icon_url="https://cdn.discordapp.com/avatars/825696493037944882/db7f3d6bbe165222fc75b50402fcfdec.webp")
+    final_message = await ctx.send(embed=page2)
     await final_message.add_reaction('◀')
     await final_message.add_reaction('▶')
-    
-    
 
 client.run(token)
 client.add_command(changeprefix)
